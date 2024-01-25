3f22ce3d22ee382db28bbdf6c107b729d808ba43
aditichhabria <aditischhabria@gmail.com>
2024-01-25 10:46:29 +0530

    firsttry

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello,World! New");
+        System.out.println("Changes");
+    }
+}
